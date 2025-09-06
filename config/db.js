04cdb32d0fc70b4d930c677201d0cafea7de78da
sagagaga4 <sagagaga4@gmail.com>
2025-09-06 17:24:02 +0300

    fixed mongoose connect db

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/app')
+        await mongoose.connect('mongodb://127.0.0.1:27017/dailycollabsdb')
         console.log('✅ Connected to MongoDB');
     }catch(error){
         console.error('MongoDB connection error',error)
